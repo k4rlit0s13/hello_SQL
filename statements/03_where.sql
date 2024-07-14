79ecf2b0b444235e9cb3c7516e5f89228de05ab2
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-14 12:04:23 -0500

    feat: :sparkles: WHERE

---
+++ statements/03_where.sql
@@ -0,0 +1,8 @@
+SELECT * FROM users WHERE age=15;                   -- selecciona todos los usuarios donde la edad sea igual a 15
+
+SELECT name_user FROM users WHERE age=15;           -- selecciona los nombres que tengan edad de 15 en usuarios
+
+SELECT DISTINCT name_user FROM users WHERE age=15;  -- selecciona los nombres con las distintas edades igual a 15  
+
+SELECT DISTINCT age FROM users WHERE age=15;        -- selecciona las distintas edades de 15
+
