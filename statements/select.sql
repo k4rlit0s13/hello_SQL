3b60dfb06092f66aa9b4dd042c0a244e4499786b
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-14 11:46:02 -0500

    feat: :sparkles: CONSULTA DE DATOS: SELECT

---
+++ statements/select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users;                            -- selecciona todos los datos de la tabla usuarios
+
+SELECT name_user FROM users;                    -- selecciona los nombres de la tabla usuarios
+
+SELECT user_id,name_user FROM users;            -- selecciona el id y nombre de la tabla usuarios
