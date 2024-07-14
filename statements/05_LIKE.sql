39d1ad02cdb33db7f439be31fe57dfacec6084f6
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-14 13:59:50 -0500

    feat: :sparkles: LIKE

---
+++ statements/05_LIKE.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE email  LIKE '%gmail.com';  -- emails que tengan gmail.com
+
+SELECT * FROM users WHERE email  LIKE 'juana%';      -- emails que comienzen con juana
+
+SELECT * FROM users WHERE email  LIKE '%@%';         -- emails que tengan @
