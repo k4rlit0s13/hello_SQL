11735e8eb953bb41ecb2c7d12f5fb6810d16d957
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-14 15:10:05 -0500

    feat: :sparkles: NOT

---
+++ statements/06_NOT.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE NOT email='carlosalberto@gmail.com';
+
+SELECT * FROM users WHERE NOT email='carlosalberto@gmail.com' AND age=15;
+
+SELECT * FROM users WHERE NOT email='carlosalberto@gmail.com' OR age=15;
