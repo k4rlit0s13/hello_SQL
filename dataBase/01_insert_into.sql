a98e6c7fd6104492b7159eeab902ae0da04b6ab8
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-16 08:05:34 -0500

    feat: :sparkles: INSERT INTO

---
+++ dataBase/01_insert_into.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id, name_user, age) VALUES (8,'Maria',25) --meter un dato nuevo, id nombre y edad
+
+INSERT INTO users  (name_user,surname) VALUES  ('Paco','Pérez') --agregar un dato nuevo solo nombre y apellido(el id recordamos que lo creamos proceduralmente)
