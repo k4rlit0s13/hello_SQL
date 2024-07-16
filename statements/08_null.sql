3ecbde2d1bf825fb7a671cca19998721ca6434cb
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-07-16 07:45:29 -0500

    feat: :checkout: IFNULL

--- statements/08_null.sql
+++ statements/08_null.sql
@@ -2,3 +2,6 @@ SELECT * FROM users WHERE email IS NULL;     --muestra los usuario con email NUL
 
 SELECT * FROM users WHERE email IS NOT NULL; --muestra los usuarios que no tengan NULL email
 
+SELECT name_user,surname, IFNULL(age,0) AS age FROM users; --IFNULL para cambiar cosas en casos si son null
+
+SELECT name_user, IFNULL(surname,'NaN') AS surname FROM users  --coloca los apellidos nulos como NaN y la columna la nombra como surname
